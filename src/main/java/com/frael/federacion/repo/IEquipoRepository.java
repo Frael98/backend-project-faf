0d18ca2a42d718d4713df9d514ea86acae048fde
Frael98 <frael1984@gmail.com>
2025-09-09 23:04:07 -0500

    Se agrega 2da forma de usar procedimientos @Procedure

--- src/main/java/com/frael/federacion/repo/IEquipoRepository.java
+++ src/main/java/com/frael/federacion/repo/IEquipoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
 import com.frael.federacion.model.Equipo;
@@ -15,5 +16,8 @@ public interface IEquipoRepository extends JpaRepository<Equipo, Integer> {
 
     @Query(value = "EXEC SP_CONSULTA_EQUIPOS :valor ", nativeQuery = true)
     public List<Equipo> findAllFilter(@Param("valor") String valor);
+
+    @Procedure //Otra forma de usar procedimientos, el nombre de la funcion debe ser la misma que el procedimiento
+    public Equipo obtenerEquipo(@Param("equipo_id") Long equipoId);
 
 }
