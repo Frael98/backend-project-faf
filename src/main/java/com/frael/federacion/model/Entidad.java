59f473465f254d14cb698b2f2649564eabe0a96e
Frael98 <frael1984@gmail.com>
2025-09-09 22:59:05 -0500

    Agregacion de metodos del ciclo de vida de Hibernate para el seteo automatico de los campos

--- src/main/java/com/frael/federacion/model/Entidad.java
+++ src/main/java/com/frael/federacion/model/Entidad.java
@@ -4,6 +4,8 @@ import java.sql.Timestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,19 +15,27 @@ import lombok.Setter;
 @MappedSuperclass
 public class Entidad {
     
-    @NotNull
     @Column(name = "createdAt", columnDefinition = "datetime default getdate()", nullable = false)
-    protected final Timestamp createdAt = new Timestamp(System.currentTimeMillis());
+    protected Timestamp createdAt;
 
-    @NotNull
     @Column(name = "updatedAt", columnDefinition = "datetime")
     protected Timestamp updatedAt;
 
-    @NotNull
     @Column(name = "deleteAt", columnDefinition = "datetime")
     protected Timestamp deletedAt;
 
     @NotNull
     @Column(name = "estado", columnDefinition = "varchar(2) default 'A'")
-    protected char estado = 'A';    
+    protected char estado = 'A';
+
+     // --- Métodos de ciclo de vida ---
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
+    }
 }
